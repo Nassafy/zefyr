46556fd88c16a8985fbda5e5064d6f3cf263a906
Matthias <matthiaspinard@gmail.com>
2019-09-07 09:50:03 +0200

    Removing default text color

--- packages/zefyr/lib/src/widgets/theme.dart
+++ packages/zefyr/lib/src/widgets/theme.dart
@@ -72,7 +72,7 @@ class ZefyrThemeData {
       fontSize: 16.0,
       height: 1.25,
       fontWeight: FontWeight.normal,
-      color: Colors.grey.shade800,
+      // // color: Colors.grey.shade800,
     );
     final padding = const EdgeInsets.only(bottom: 16.0);
     final boldStyle = new TextStyle(fontWeight: FontWeight.bold);
@@ -174,7 +174,7 @@ class HeadingTheme {
       level1: StyleTheme(
         textStyle: TextStyle(
           fontSize: 30.0,
-          color: Colors.grey.shade800,
+          // color: Colors.grey.shade800,
           height: 1.25,
           fontWeight: FontWeight.w600,
         ),
@@ -183,7 +183,7 @@ class HeadingTheme {
       level2: StyleTheme(
         textStyle: TextStyle(
           fontSize: 24.0,
-          color: Colors.grey.shade800,
+          // color: Colors.grey.shade800,
           height: 1.25,
           fontWeight: FontWeight.w600,
         ),
@@ -192,7 +192,7 @@ class HeadingTheme {
       level3: StyleTheme(
         textStyle: TextStyle(
           fontSize: 20.0,
-          color: Colors.grey.shade800,
+          // color: Colors.grey.shade800,
           height: 1.25,
           fontWeight: FontWeight.w600,
         ),
